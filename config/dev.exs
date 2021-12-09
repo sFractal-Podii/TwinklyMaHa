4d5ecdde18f9d453947b92a25ac7ad9ecf330d19
TraceyOnim <traceyonim22@gmail.com>
2021-12-09 12:17:58 +0300

    use correct option

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,7 @@ config :twinkly_maha, TwinklyMahaWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch-options-stdin",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
