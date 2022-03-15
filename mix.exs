d05433c3fcbb3d98bda178b8f698719967a11197
WanjikuMac <edwardscaroline32@gmail.com>
2022-03-15 10:05:33 +0300

    return version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TwinklyMaha.MixProject do
   def project do
     [
       app: :twinkly_maha,
-      version: "0.7.4",
+      version: "0.7.3",
       elixir: "~> 1.11.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
