e68caf51882539b0d41d994b105084ae893d6640
sigu <sigumagwa@gmail.com>
2025-03-04 14:32:26 +0300

    increase version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TwinklyMaha.MixProject do
   def project do
     [
       app: :twinkly_maha,
-      version: "0.13.6",
+      version: "0.13.6-dev",
       elixir: "~> 1.17.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
