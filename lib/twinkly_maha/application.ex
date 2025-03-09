e4d98e1e195eb475446807a82974ce70785ad2a3
sigu <sigumagwa@gmail.com>
2025-03-09 10:59:14 +0300

    check lint first

--- lib/twinkly_maha/application.ex
+++ lib/twinkly_maha/application.ex
@@ -1,25 +1,17 @@
 defmodule TwinklyMaha.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Start the Telemetry supervisor
       TwinklyMahaWeb.Telemetry,
-      # Start the PubSub system
       {Phoenix.PubSub, name: TwinklyMaha.PubSub},
-      # Start the Endpoint (http/https)
       TwinklyMahaWeb.Endpoint,
-      # start mqtt connection
       Emqtt.Emqx,
       Emqtt.Hivemq
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: TwinklyMaha.Supervisor]
     Supervisor.start_link(children, opts)
   end
