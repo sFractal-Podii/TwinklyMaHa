5cf44788f2af3a9d97fbd512ac67015f1dd408bc
Sigu Magwa <sigumagwa@gmail.com>
2025-07-01 16:28:54 +0300

    bump to alpha

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TwinklyMaha.MixProject do
   def project do
     [
       app: :twinkly_maha,
-      version: "0.13.6-dev",
+      version: "0.13.6-alpha",
       elixir: "~> 1.17.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
